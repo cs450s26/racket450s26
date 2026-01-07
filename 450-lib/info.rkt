06c6bbca375504266595fc6c96253435b4fb15ed
Stephen Chang <stephen.chang@umb.edu>
2026-01-07 15:41:57 -0500

    add init files

---
+++ 450-lib/info.rkt
@@ -0,0 +1,13 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps
+  '(("base" #:version "8.0")))
+
+(define build-deps '())
+
+(define pkg-desc "Racket for CS 450")
+(define pkg-authors '(stchang))
+
+(define version "0.1")
---
+++ 450-lib/racket450/main.rkt
@@ -0,0 +1,6 @@
+#lang racket/base
+
+(provide (all-from-out racket/base
+                       racket/string))
+
+(require racket/string)
