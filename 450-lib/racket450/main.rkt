221512b4ef0552cd155197c542938961338b48b5
Stephen Chang <stephen.chang@umb.edu>
2026-01-28 10:55:30 -0500

    automatically insert #:transparent in struct; disallow some htdp forms

--- 450-lib/racket450/main.rkt
+++ 450-lib/racket450/main.rkt
@@ -16,13 +16,18 @@
           ;; excluded names:
           #%module-begin
           set!
-          define-values
+          struct
           values
+          define-values
 ;          match-define
 ;          match-let
 ;          let*
           letrec)
+         local
+         check-expect
+         define-struct
          (rename-out
+          [struct450 struct]
           [mb450 #%module-begin]))
 
 (require racket/bool
@@ -48,6 +53,30 @@
         do-provide-all
         x ...)]))
 
-;; override these htdp forms with err
-;; - local
-;; - check-expect
+;; override htdp forms with stxerr
+
+(define-syntax define-struct
+  (syntax-parser
+    [(_ . _)
+     #'(raise-syntax-error
+        'define-struct "use struct instead of define-struct")]))
+
+(define-syntax check-expect
+  (syntax-parser
+    [(_ . _)
+     #'(raise-syntax-error
+        'check-expect "use check-equal? instead of check-expect")]))
+
+(define-syntax local
+  (syntax-parser
+    [(_ . _)
+     #'(raise-syntax-error
+        'local "use let or let* instead of local")]))
+
+;; inserts #:transparent if not already present
+(define-syntax struct450
+  (syntax-parser
+    [(_ (~and x (~not :keyword)) ...
+        (~optional (~and #:transparent tr:keyword)
+                   #:defaults ([tr #'#:transparent])))
+     #'(struct x ... tr)]))
